1c967794a8549c4f761017c32118d80010ff7fe8
Jason Plank <plank.jason@outlook.com>
2024-11-09 13:43:14 +0200

    Moved project into a solution.
    Added Identity, EFCore and created tabled to used for authentication.

--- DRSAlert.API/ApplicationDBContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace DRSAlert.API;
-
-public class ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : DbContext(options)
-{
-    // DbSets go here
-}
